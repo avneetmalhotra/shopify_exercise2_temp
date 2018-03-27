eeaf94b1724734c9b385e16faa4432ce029cb3ff
Avneet Singh Malhotra <avneet@vinsol.com>
2018-03-27 19:42:07 +0530

    Create discount-input-tag.js

---
+++ discount-input-tag.js
@@ -0,0 +1,6 @@
+$(function(){
+  
+  var discount_input_element = "<div>{% if customer and customer.metafields.global.advance_discount_code %}<input class='js-form-discount' type='hidden' name='discount' value='{{ customer.metafields.global.advance_discount_code }}' >{% endif %}</div>"
+  $("form[action='/cart'][method='post']").append(discount_input_element);
+  
+});
